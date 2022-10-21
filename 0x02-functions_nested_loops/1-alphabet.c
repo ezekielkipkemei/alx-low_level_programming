d2b0dcac6ca711c370813f83e5c928cec5f24072
ezekielkipkemei <kemeikenei0002@gmail.com>
2022-10-21 06:39:55 +0000

    debugging

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#ifndef MAIN_H
 /*prints all alphabet in lower case*/
-int main(void)
+int putchar(char c)
 {
 /*declare a variable*/
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
 /* print the value of the variable*/
 putchar(alphabet);
-}
 putchar('\n');
 return (0);
 }
