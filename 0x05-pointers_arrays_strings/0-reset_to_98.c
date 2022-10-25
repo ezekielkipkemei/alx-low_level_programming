c972f927662cf8b87bf0fd9963ebe017d69f0afa
ezekielkipkemei <kemeikenei0002@gmail.com>
2022-10-25 08:51:35 +0000

    task 2 modified

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- *reset_to_98 -> a function to update the value it points to 98
- *@n: pointer to n to be updated
- */
-void reset_to_98(int *n)
-{
-	*n = 98;
-}
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,19 @@
-#include "stdio.h"
+#include "main.h"
 /**
-*_strlen -> function to get the length of a string
-*@s: string pointer to passed to this function
-*Return: returns length of the string
-*/
+ * _strlen - finds and outputs length of string
+ * @s: pointer to string.
+ *
+ * Return: length of a string.
+ */
 int _strlen(char *s)
 {
-return (_strlen(s));
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
 }
